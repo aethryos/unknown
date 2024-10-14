0150f07d39625dbf90649eec4b7dd7f4c5777a40
aethryos <aethryon@gmail.com>
2024-10-14 19:01:50 +0500

    Update placer.js

--- placer.js
+++ placer.js
@@ -1,44 +1,58 @@
-function placeTrap(ca, enemy, distance, direction) {
-  // Calculate the trap direction based on the enemy's direction
-  const trapDirection = direction + (Math.random() - 0.5) * Math.PI / 4;
-
-  // Place the trap at a random distance from the enemy
-  const trapX = enemy.x + Math.cos(trapDirection) * (distance + Math.random() * 20);
-  const trapY = enemy.y + Math.sin(trapDirection) * (distance + Math.random() * 20);
-
-  // Place the trap
-  this.place(4, trapX, trapY);
-}
-
-function placeSpike(ca, enemy, distance, direction) {
-  // Calculate the spike direction based on the enemy's direction
-  const spikeDirection = direction + (Math.random() - 0.5) * Math.PI / 4;
-
-  // Place the spike at a random distance from the enemy
-  const spikeX = enemy.x + Math.cos(spikeDirection) * (distance + Math.random() * 20);
-  const spikeY = enemy.y + Math.sin(spikeDirection) * (distance + Math.random() * 20);
-
-  // Place the spike
-  this.place(7, spikeX, spikeY);
-}
-
-if (ca.enemy && O[2].enabled) {
-  const enemyDistance = s.distance(this, ca.enemy);
-  const enemyDirection = s.direction(ca.enemy, this);
+if (ca.enemy && O[2].enabled) { 
+  const enemyDistance = s.distance(this, ca.enemy); 
+  const enemyDirection = s.direction(ca.enemy, this); 
   const dist = +document.querySelector("#chat-0").value;
 
-  if (enemyDistance <= dist) {
-    const trapEntity = ca.entities.find(a => a && s.distance(a, ca.enemy) < 60 && a.type === 6 && this.mine(a));
-
+  if (enemyDistance <= dist) { 
+    const trapEntity = ca.entities.find(a => a && s.distance(a, ca.enemy) < 60 && a.type === 6 && this.mine(a)); 
+    const trapDirection = trapEntity ? s.direction(trapEntity, this) : null;
     if (trapEntity) {
-      // Place a spike near the trap
-      placeSpike(ca, ca.enemy, 30, enemyDirection);
+      const angleDiff = Math.abs(enemyDirection - trapDirection);
+      const isEnemyInFront = angleDiff < Math.PI / 2;
 
-      // Try to break the trap
-      this.place(4, trapEntity.x, trapEntity.y);
+      if (isEnemyInFront) {
+        this.place(4, this.x + Math.cos(enemyDirection) * 70, this.y + Math.sin(enemyDirection) * 70);
+      } else {
+        const behindDirection = (enemyDirection + Math.PI) % (2 * Math.PI);
+        this.place(4, this.x + Math.cos(behindDirection) * 50, this.y + Math.sin(behindDirection) * 50);
+      }
+
+      // Aggressive angle calculations
+      const angleStep = Math.PI / 12;
+      const angles = Array.from({ length: 36 }, (_, i) => enemyDirection + (i - 18) * angleStep);
+      const scores = angles.map(angle => ({
+        angle,
+        score: s.distance(ca.enemy, this, angle) * Math.cos(s.direction(ca.enemy, this, angle) - enemyDirection)
+      }));
+
+      const topScores = quickselect(scores, 15);
+      const validAngles = topScores.map(({ angle }) => angle);
+
+      ca.angles = validAngles;
+      this.place(4, ...validAngles);
+
+      // Additional aggressive trap placement
+      const spamAngles = Array.from({ length: 15 }, (_, i) => enemyDirection + (i - 7) * angleStep);
+      this.place(4, ...spamAngles);
+
+      // Adaptive trap placement
+      const adaptAngles = Array.from({ length: 10 }, (_, i) => enemyDirection + (i - 5) * angleStep);
+      const adaptScores = adaptAngles.map(angle => ({
+        angle,
+        score: s.distance(ca.enemy, this, angle) * Math.cos(s.direction(ca.enemy, this, angle) - enemyDirection)
+      }));
+
+      const topAdaptScores = quickselect(adaptScores, 5);
+      const adaptValidAngles = topAdaptScores.map(({ angle }) => angle);
+
+      this.place(4, ...adaptValidAngles);
+
+      // Spike placement
+      const spikeAngles = Array.from({ length: 10 }, (_, i) => enemyDirection + (i - 5) * angleStep);
+      this.place(7, ...spikeAngles);
     } else {
-      // Place a trap near the enemy
-      placeTrap(ca, ca.enemy, 50, enemyDirection);
+      ca.angles = [enemyDirection];
+      this.place(7, enemyDirection);
     }
   }
 }
