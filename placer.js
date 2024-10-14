0b8742f388ffe9d4bda2d438548a721b9c8f9551
aethryos <aethryon@gmail.com>
2024-10-14 20:16:40 +0500

    Update placer.js

--- placer.js
+++ placer.js
@@ -3,13 +3,22 @@ if (ca.enemy && O[2].enabled) {
   const enemyDirection = s.direction(ca.enemy, this);
   const dist = +document.querySelector("#chat-0").value;
 
+  console.log("Enemy distance:", enemyDistance);
+  console.log("Enemy direction:", enemyDirection);
+
   if (enemyDistance <= dist) {
     const trapEntity = ca.entities.find(a => a && s.distance(a, ca.enemy) < 60 && a.type === 6 && this.mine(a));
     const trapDirection = trapEntity ? s.direction(trapEntity, this) : null;
 
+    console.log("Trap entity:", trapEntity);
+    console.log("Trap direction:", trapDirection);
+
     if (trapEntity) {
       const angleDiff = Math.abs(enemyDirection - trapDirection);
       const isEnemyInFront = angleDiff < Math.PI / 2;
+
+      console.log("Angle diff:", angleDiff);
+      console.log("Is enemy in front:", isEnemyInFront);
 
       if (isEnemyInFront) {
         this.place(4, this.x + Math.cos(enemyDirection) * 70, this.y + Math.sin(enemyDirection) * 70);
@@ -26,8 +35,14 @@ if (ca.enemy && O[2].enabled) {
         score: s.distance(ca.enemy, this, angle) * Math.cos(s.direction(ca.enemy, this, angle) - enemyDirection)
       }));
 
+      console.log("Angles:", angles);
+      console.log("Scores:", scores);
+
       const topScores = quickselect(scores, 15);
       const validAngles = topScores.map(({ angle }) => angle);
+
+      console.log("Top scores:", topScores);
+      console.log("Valid angles:", validAngles);
 
       ca.angles = validAngles;
       this.place(4, ...validAngles);
@@ -43,13 +58,19 @@ if (ca.enemy && O[2].enabled) {
         score: s.distance(ca.enemy, this, angle) * Math.cos(s.direction(ca.enemy, this, angle) - enemyDirection)
       }));
 
+      console.log("Adapt angles:", adaptAngles);
+      console.log("Adapt scores:", adaptScores);
+
       const topAdaptScores = quickselect(adaptScores, 5);
       const adaptValidAngles = topAdaptScores.map(({ angle }) => angle);
+
+      console.log("Top adapt scores:", topAdaptScores);
+      console.log("Adapt valid angles:", adaptValidAngles);
 
       this.place(4, ...adaptValidAngles);
 
       // Spike placement
-      const spikeAngles = Array.from({ length: 10 }, (_, i) => enemyDirection + (i - 5) * angleStep);
+      const spikeAngles = Array.from({ length: 10 }, (_, i) => enemyDirection + (i - 5) * angleStep );
       this.place(7, ...spikeAngles);
     } else {
       ca.angles = [enemyDirection];
