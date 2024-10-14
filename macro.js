1e605ac72cc9cd9371d91d37135d6d4623dd3595
aethryos <aethryon@gmail.com>
2024-10-14 15:34:01 +0500

    Update macro.js

--- macro.js
+++ macro.js
@@ -5,23 +5,9 @@ const ga = {
   mill: new x(5, 78, 30)
 };
 
-let debounceTimeout = null;
-let throttleTimeout = null;
-
 document.addEventListener("keydown", a => {
   const b = a.keyCode;
-  clearTimeout(debounceTimeout);
-  debounceTimeout = setTimeout(() => {
-    const properties = Object.keys(ga);
-    let index = 0;
-
-    throttleTimeout = setInterval(() => {
-      if (index < properties.length) {
-        ga[properties[index]].start(b);
-        index++;
-      } else {
-        clearInterval(throttleTimeout);
-      }
-    }, 50); // Adjust the throttle delay as needed
-  }, 300); // Adjust the debounce time as needed
+  for (let c in ga) {
+    ga[c].start(b);
+  }
 });
